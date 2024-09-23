49ad331b3a5e9c4b7f898c005f63cf85cb8412be
Toh bi <iriefrancktohbi@gmail.com>
2024-09-23 10:36:36 +0000

    Ajout de nouvelle fonctionnalité

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session,send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
 
@@ -52,6 +52,22 @@ def get_data(filepath):
     df = pd.read_csv(filepath, sep=',')
     return df
 
+# Route pour récupérer les départements en fonction de la région sélectionnée
+@app.route('/get_departements/<region>')
+def get_departements(region):
+    df = get_data("region_poro1.csv")  # Charger les données
+    df_filtered = df[df['nom_region'] == region]
+    departements = df_filtered['nom_departement'].sort_values().unique()
+    return jsonify(list(departements))
+
+# Route pour récupérer les sous-préfectures en fonction du département sélectionné
+@app.route('/get_sous_prefectures/<departement>')
+def get_sous_prefectures(departement):
+    df = get_data("region_poro1.csv")  # Charger les données
+    df_filtered = df[df['nom_departement'] == departement]
+    sous_prefectures = df_filtered['nom_sousprefecture'].sort_values().unique()
+    return jsonify(list(sous_prefectures))
+
 # Route pour afficher la page de requête
 @app.route('/request_indicateur', methods=['GET', 'POST'])
 def request_indicateur():
@@ -74,6 +90,7 @@ def request_indicateur():
 
         # Commencer avec l'ensemble complet des données
         df_filtered = df.drop(columns=['statut','id'], errors='ignore')
+        df_filtered =df_filtered[df_filtered['nom_indicateur'] == indicateur_SELECT]
 
         # Appliquer chaque filtre uniquement si une valeur est sélectionnée
         if sousprefecture_SELECT:
@@ -82,12 +99,22 @@ def request_indicateur():
             df_filtered = df_filtered.drop(columns=['nom_region', 'nom_departement'], errors='ignore')
         # Si le département est sélectionné (et pas de sous-préfecture), on filtre par département
 # et on supprime la colonne région
-        elif departement_SELECT:
-            df_filtered = df_filtered[df_filtered['nom_departement'] == departement_SELECT]
+        elif departement_SELECT and not sousprefecture_SELECT:
+            df_filtered = df_filtered[
+                (df_filtered['nom_region'] == region_SELECT) &
+                (df_filtered['nom_departement'].isna()) 
+
+            ]
+            
             # Supprimer la colonne 'nom_region' si le département est sélectionné
             df_filtered = df_filtered.drop(columns=['nom_region'], errors='ignore')
-        elif region_SELECT:
-            df_filtered = df_filtered[df_filtered['nom_region'] == region_SELECT]
+        elif region_SELECT and not departement_SELECT and not sousprefecture_SELECT:
+            df_filtered = df_filtered[
+                (df_filtered['nom_region'] == region_SELECT) &
+                (df_filtered['nom_departement'].isna()) &
+                (df_filtered['nom_sousprefecture'].isna())
+            ]
+
 
         # Supprimer les colonnes contenant uniquement des valeurs NaN
         df_filtered = df_filtered.dropna(axis=1, how='all')
@@ -103,9 +130,10 @@ def request_indicateur():
 
             # Obtenir les colonnes disponibles pour la zone de dépôt
             available_columns = list(df_filtered.columns)
+        
 
             # Afficher la page résultat avec les colonnes disponibles et sans message d'erreur
-            return render_template('result.html', available_columns=available_columns, message="")
+            return render_template('result.html', available_columns=available_columns, message="",indicateur_SELECT=indicateur_SELECT)
 
     # Si GET, afficher la page de sélection avec les options de filtre
     return render_template(
@@ -122,6 +150,7 @@ def request_indicateur():
 def generate_pivot_table():
     data = request.get_json()
     selected_columns = data.get('columns', [])
+    
 
     # Reload filtered dataframe from session
     df_filtered_json = session.get('df_filtered', None)
@@ -131,20 +160,45 @@ def generate_pivot_table():
         
         if selected_columns:
             # Generate the pivot table based on selected columns
-            pivot_table = pd.pivot_table(df, values='Valeur', index=selected_columns)
+            pivot_table = pd.pivot_table(df, values='Valeur', index=selected_columns,aggfunc='sum')
             pivot_html = pivot_table.to_html(classes='table table-bordered')
 
             return jsonify({"table_html": pivot_html})
 
     return jsonify({"table_html": "<p>Aucune colonne sélectionnée</p>"})
 
+import io
 
+# Pour télécharger le tableau en Excel
+@app.route('/download_excel')
+def download_excel():
+    selected_columns = request.args.get('columns', '').split(',')
 
+    # Vérifier si des colonnes ont été sélectionnées
+    if not selected_columns or selected_columns == ['']:
+        return "<p>Aucune variable n'a été sélectionnée pour le téléchargement</p>"
 
+    # Reload filtered dataframe from session
+    df_filtered_json = session.get('df_filtered', None)
 
+    if df_filtered_json:
+        df = pd.read_json(df_filtered_json)
 
+        # Générer le tableau croisé dynamique en utilisant les colonnes sélectionnées
+        pivot_table = pd.pivot_table(df, values='Valeur', index=selected_columns, aggfunc='sum')
 
+        # Créer un fichier Excel en mémoire
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            pivot_table.to_excel(writer, index=True)
 
+        # Déplacer le curseur au début du fichier
+        output.seek(0)
+
+        # Retourner le fichier Excel à télécharger
+        return send_file(output, download_name='tableau_pivot.xlsx', as_attachment=True)
+    else:
+        return "<p>Aucune donnée disponible pour téléchargement</p>"
 
 
 
