bb1f5ac127f31ad65e086cbc681154e255181462
Toh bi <iriefrancktohbi@gmail.com>
2024-10-05 18:59:53 +0000

    Finalisation du dash

--- test.py
+++ test.py
@@ -1,14 +1,47 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template,jsonify
 import random
 
 app = Flask(__name__)
 
-# Route pour afficher la page HTML (index)
 @app.route('/')
 def index():
     min_year = 2019
-    max_year = 2027
-    return render_template('test.html', min=min_year, max=max_year)
+    max_year = 2040
+
+    # Initialisation des listes de données
+    data_commercants = []
+    data_esperance_vie = []
+    data_ratio_eleve_primaire = []
+
+    # Générer les valeurs pour chaque année
+    annees = list(range(min_year, max_year + 1))
+    for annee in annees:
+        data_commercants.append({
+            'indicateur': 'Nombre de commerçants',
+            'annee': annee,
+            'valeur': round(random.randint(200, 2000))
+        })
+
+        data_esperance_vie.append({
+            'indicateur': 'Espérance de vie à la naissance',
+            'annee': annee,
+            'valeur': round(random.randint(40, 130))
+        })
+
+        data_ratio_eleve_primaire.append({
+            'indicateur': 'Ratio élève par salle primaire',
+            'annee': annee,
+            'valeur': round(random.uniform(20, 80), 2)
+        })
+
+    # Passer les données au template
+    return render_template('test.html', min=min_year, max=max_year, 
+                           data_commercants=data_commercants, 
+                           data_esperance_vie=data_esperance_vie,
+                           data_ratio_eleve_primaire=data_ratio_eleve_primaire
+                           )
+
+
 
 
 
@@ -22,9 +55,17 @@ def index():
 def get_data():
     data = []
     
+    min_year = 2019
+    max_year = 2040
+
+  
+
+    # Générer les valeurs pour chaque année
+    annees = list(range(min_year, max_year + 1))
+    
     # Générer les données pour l'indicateur "Production de cultures"
     cultures = ['Cacao', 'Café', 'Coton']
-    annees = [2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027]
+
 
     for annee in annees:
         for culture in cultures:
@@ -37,7 +78,7 @@ def get_data():
             
     # Générer les données pour l'indicateur "Effectif de la population"
     sexes = ['M', 'F']
-    groupes_age = ['0-4', '5-9', '10-14', '15-19', '20-24','25-29','30-34','35-39','40-44','45-49']
+    groupes_age = ['0-4', '5-9', '10-14', '15-19', '20-24','25-29','30-34','35-39','40-44','45-49','50-54','55-59','60-64']
 
     for annee in annees:
         for groupe in groupes_age:
@@ -99,6 +140,14 @@ def get_data():
                     'cycle_scolaire': cycle,
                     'valeur': round(random.uniform(60, 100), 2)  # Taux entre 60% et 100%
                 })
+    #Nombre de commercant 
+    for annee in annees:
+        data.append({
+            'indicateur': 'Nombre de commerçants',
+            'annee': annee,
+            'valeur': round(random.uniform(60, 100), 2)  # Taux entre 60% et 100%
+        })
+
 
     return jsonify(data)
 
